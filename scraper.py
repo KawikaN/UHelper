d7baf263231752dcc0bf70b771a4fa6b4b8fe398
Charles <creidbrowniii@gmail.com>
2024-05-05 01:47:44 -1000

    really close to finishing scraper

--- scraper.py
+++ scraper.py
@@ -19,7 +19,6 @@ def scrape_professors():
    url = "https://www.ratemyprofessors.com/search/professors/1106?q=*"
    driver.implicitly_wait(0) # driver will wait 0 seconds for elements to load
    driver.get(url) # opening url with selenium chrome
-   
 
    # simulate clicking "close" on cookies popup
    try: # using a try catch just incase close is not present
@@ -30,13 +29,14 @@ def scrape_professors():
       print("Close button not found or already closed.", e) # prints error message
 
    # simulate clicking "show more" button
-   delay = 0.55 # seconds
+   delay = 0.6 # seconds
    professor_count = 0
    while professor_count <= 3806:
       try: # checking if we can load more teachers
          show_more_button = WebDriverWait(driver, 10, poll_frequency=0.1).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Show More')]")))
          if(show_more_button): # waits until the element is located in html
-            WebDriverWait(driver, 100, poll_frequency=delay).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Show More')]"))).click() # waits until element is clickable and clicks it
+            button = WebDriverWait(driver, 100, poll_frequency=delay).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Show More')]"))) # waits until element is clickable 
+            driver.execute_script("arguments[0].click()", button) # clicks button
             print(f"button clicked - {professor_count}")
             professor_count += 8
       except TimeoutException:
@@ -53,23 +53,19 @@ def scrape_professors():
    professor_list = []
    professors = soup.find_all('a', class_='TeacherCard__StyledTeacherCard-syjs0d-0 dLJIlx') # finds all the cards for teachers in html and stores into array
    for prof in professors: # iterates over professor tags saved in list
-      # gotta somehow get tags for rating and difficulty(div tags are different for each teacher)
       name = prof.find('div', class_="CardName__StyledCardName-sc-1gyrgim-0 cJdVEK").text # saves the text(professor name)
-      rating = prof.find('div', class_="CardFeedback__CardFeedbackItem-lq6nix-1 fyKbws").text # saves the text(professor rating)
-      feedback_class_name = "CardFeedback__CardFeedbackItem-lq6nix-1 fyKbws"
-      difficulty = prof.find('div', class_=feedback_class_name).find('div', 
-         class_="CardFeedback__CardFeedbackNumber-lq6nix-2 hroXqf").text
-      take_again_percent = prof.find('div', class_=feedback_class_name).find('div',
-         class_="CardFeedback__CardFeedbackNumber-lq6nix-2 hroXqf").text
-      professor_list.append({'name': name, 'rating': rating, 'difficulty': difficulty, 'take_again': take_again_percent}) # saves scrapped data to an array
-      print(f"Professor: {name}, Rating: {rating}, Difficulty: {difficulty}, Take Again?: {take_again_percent}") 
+      rating = prof.find('div', class_="CardNumRating__StyledCardNumRating-sc-17t4b9u-0 eWZmyX").text # saves the text(professor rating)
+      dept = prof.find('div', class_="CardSchool__Department-sc-19lmz2k-0 haUIRO").text # saves professor's department
+      data_class_name = "CardFeedback__CardFeedbackNumber-lq6nix-2 hroXqf"
+      feedback_tag = prof.find('div', class_=data_class_name).contents[0] # feedback includes difficulty & take again %
+      feedback = feedback_tag.contents
+      professor_list.append({'name': name, 'rating': rating, 'feedback': feedback}) # saves scrapped data to an array
+      print(f"Professor: {name}, Rating: {rating}, Department: {dept}, Feedback: {feedback}") 
 
    driver.quit()
 
    # convert list to pandas dataframe and save it as csv file
    df = pd.DataFrame(professor_list) 
    df.to_csv("professors.csv")
-   # print(df, flush=True)
-   # print("", flush = True) # required to be able to print to terminal while using Flask
 
 scrape_professors()
